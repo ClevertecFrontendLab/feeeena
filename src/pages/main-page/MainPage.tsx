b130cf420846033b6f5c6751424823da204ec3fc
Lina <linabrinsi.n@gmail.com>
2024-02-11 18:00:53 +0300

    add style to sider

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-import { Layout } from 'antd';
-const { Sider, Header, Content, Footer } = Layout;
+import { Button, Layout } from 'antd';
+const { Sider, Header, Content } = Layout;
 
 import './mainPage.css';
+import "@components/sider/sider.css"
 
 import HeaderBox from '@components/header/HeaderBox';
-import SiderBar from '@components/sider/SiderBar';
+
 import ContentActions from '@components/content/ContentActions';
 import ContentWrap from '@components/content/ContentWrap';
 import ContentCards from '@components/content/ContentCards';
 import FooterBox from '@components/footer/FooterBox';
 
- const MainPage: React.FC = () => {
+import {MenuFoldOutlined,MenuUnfoldOutlined } from '@ant-design/icons';
+import Logo from "@components/sider/Logo.tsx"
+import MenuList from '@components/sider/MenuList';
+const logoExpanded = "./public/Logo.png";
+const logoCollapsed = "./public/fit.png";
+
+
+ const MainPage: React.FC = () => { 
+  const [collapsed, setCollapsed] = useState(false);
    return (
-    <Layout className="main-page" style={{ minHeight: '100vh' }}>
-      <Sider><SiderBar/></Sider>
+    <Layout className={`sider-layout main-page`} style={{ minHeight: '100vh' }}>
+        <Sider trigger={null} collapsible collapsed={collapsed} className="sider-bar">
+          <Logo src={collapsed ? logoCollapsed : logoExpanded} />
+          <MenuList />
+        </Sider>
         <Layout >
           <Header className='header-content'><HeaderBox /></Header>
-            <Content ><ContentActions/>
+            <Content>
+              <Button
+              type='text'
+              className={`trigger trigger-col`}
+              onClick={() => setCollapsed(!collapsed)}
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              />
+              <ContentActions/>
+              
               <ContentWrap/>
               <ContentCards/>
               <FooterBox/>
@@ -26,6 +46,9 @@ import FooterBox from '@components/footer/FooterBox';
               
         </Layout>
       </Layout>
+   
+        
+     
     );
 };
 export default MainPage;
